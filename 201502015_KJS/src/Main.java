294d0ded6bdd006402d1dfcdd8dc371796532c49
wotmd <wotmd8534@naver.com>
2019-06-19 12:13:24 +0000

    description add

--- 201502015_KJS/src/Main.java
+++ 201502015_KJS/src/Main.java
@@ -16,8 +16,8 @@ public class Main {
             try {
                 System.out.println("… " + interpreter.runCommand(input));   // 해당되는 결과 출력
             } catch (UndefinedException e){
-                System.out.println(e.getMessage());
-            } catch (Exception e){
+                System.out.println(e.getMessage()); //  정의되지않은 변수, 함수 오류 처리
+            } catch (Exception e){         // 문법 오류, 또는 잘못된 계산의 오류 처리
                 System.out.println("SyntaxError: invalid syntax");
             }
         }
